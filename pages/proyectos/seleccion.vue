a63a33ef896100bf684e4f87acdc03cb3bcd01e3
Lunit567 <martin.lun.567@gmail.com>
2023-08-30 01:49:34 -0600

    cambio de estado de proyecto

--- pages/proyectos/seleccion.vue
+++ pages/proyectos/seleccion.vue
@@ -5,7 +5,7 @@
                 <v-card-title>
                     {{proyecto.nombre}}
                 </v-card-title>
-                <v-card-text>
+                <v-card-text v-if="estado === 'Activo'">
                     Objetivos: {{proyecto.objetivos}}
                     <v-spacer />
                     Carrera: {{carrera}}
@@ -18,12 +18,25 @@
                     <v-spacer />
                     Cupos: {{proyecto.alumnos}}
                 </v-card-text>
+                <v-card-text v-if="estado === 'Terminado'">
+                    Objetivos: {{proyecto.objetivos}}
+                    <v-spacer />
+                    Carrera: {{carrera}}
+                    <v-spacer />
+                    Encargado: {{maestro}}
+                    <v-spacer />
+                    Fecha de inicio: {{proyecto.fechainicio}}
+                    <v-spacer />
+                    Fecha de termino: {{proyecto.fechafinal}}
+                    <v-spacer />
+                    Estado: {{estado}}
+                </v-card-text>
                 <v-card-actions>
                     <v-spacer />
                     <v-btn @click="cancelar()" color="red">
                         Cancelar
                     </v-btn>
-                    <ConfirmDialog v-if="roles.rol == 'alumno' && proyecto.alumnos != 0" :description="`¿Está seguro de querer unirce al proyecto '${proyecto.nombre}'? Esta acción no se puede deshacer.`" />
+                    <ConfirmDialog v-if="roles.rol === 'alumno' && proyecto.alumnos !== 0 && estado === 'Activo'" :description="`¿Está seguro de querer unirce al proyecto '${proyecto.nombre}'? Esta acción no se puede deshacer.`" />
                 </v-card-actions>
             </v-card>
         </v-form>
@@ -45,6 +58,7 @@ export default {
         maestro: "",
         carrera: "",
         id: "",
+        estado: "",
     }),
 
     async beforeMount() {
@@ -60,6 +74,7 @@ export default {
             this.proyecto = response.data.data
             this.carrera = this.proyecto.Carrera.nombre
             this.maestro = this.proyecto.encargado.nombre
+            this.estado = this.proyecto.statuses[0].Estado
             const responseR = await this.$axios.get('/login')
             this.roles = responseR.data
         } catch (error) {
